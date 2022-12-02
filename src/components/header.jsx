c426fe53d39f12e6bc3f840fadfecf4912ed33b6
zaurbkv <zaurbkv@mail.ru>
2022-12-02 23:40:38 +0300

    newcommit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -16,7 +16,7 @@ function Header() {
           </Link>
           <ul className="cart">
             <Link to="/cart">
-              <li className="cart-item">КОРЗИНА</li>
+              <li className="cart-item">🛒</li>
             </Link>
           </ul>
         </div>
@@ -25,32 +25,32 @@ function Header() {
         <ul className="menu">
           <li
             onClick={() => setIndex(0)}
-            className={index === 0 ? 'menu__item-focused' : ''}>
+            className={index === 0 ? 'menu__item' : ''}>
             Все
           </li>
           <li
             onClick={() => setIndex(1)}
-            className={index === 1 ? 'menu__item-focused' : ''}>
+            className={index === 1 ? 'menu__item' : ''}>
             Мясные
           </li>
           <li
             onClick={() => setIndex(2)}
-            className={index === 2 ? 'menu__item-focused' : ''}>
+            className={index === 2 ? 'menu__item' : ''}>
             Вегетерианская
           </li>
           <li
             onClick={() => setIndex(3)}
-            className={index === 3 ? 'menu__item-focused' : ''}>
+            className={index === 3 ? 'menu__item' : ''}>
             Гриль
           </li>
           <li
             onClick={() => setIndex(4)}
-            className={index === 4 ? 'menu__item-focused' : ''}>
+            className={index === 4 ? 'menu__item' : ''}>
             Острые
           </li>
           <li
             onClick={() => setIndex(5)}
-            className={index === 5 ? 'menu__item-focused' : ''}>
+            className={index === 5 ? 'menu__item' : ''}>
             Закрытые
           </li>
         </ul>
