d532bc4736d911801a70e0d1a2da17303a75565e
zaurbkv <zaurbkv@mail.ru>
2022-12-02 23:26:12 +0300

    fixed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,23 +2,60 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
+  const [index, setIndex] = React.useState(0);
   return (
-    <header>
-      <div className="header__inner">
-        <Link to="/" className="logo__link">
-          <img
-            src="https://www.nicepng.com/png/full/948-9484754_crave-pizza-gif-pizza-pixel-art.png"
-            className="logo__img"
-          />
-          <span>PIXELPIZZA</span>
-        </Link>
-        <ul className="menu">
-          <Link to="/cart">
-            <li className="menu__item">КОРЗИНА</li>
+    <>
+      <header>
+        <div className="header__inner">
+          <Link to="/" className="logo__link">
+            <img
+              src="https://www.nicepng.com/png/full/948-9484754_crave-pizza-gif-pizza-pixel-art.png"
+              className="logo__img"
+            />
+            <span>PIXELPIZZA</span>
           </Link>
+          <ul className="cart">
+            <Link to="/cart">
+              <li className="cart-item">КОРЗИНА</li>
+            </Link>
+          </ul>
+        </div>
+      </header>
+      <nav className="nav">
+        <ul className="menu">
+          <li
+            onClick={() => setIndex(0)}
+            className={index === 0 ? 'menu__item-focused' : ''}>
+            Все
+          </li>
+          <li
+            onClick={() => setIndex(1)}
+            className={index === 1 ? 'menu__item-focused' : ''}>
+            Мясные
+          </li>
+          <li
+            onClick={() => setIndex(2)}
+            className={index === 2 ? 'menu__item-focused' : ''}>
+            Вегетерианская
+          </li>
+          <li
+            onClick={() => setIndex(3)}
+            className={index === 3 ? 'menu__item-focused' : ''}>
+            Гриль
+          </li>
+          <li
+            onClick={() => setIndex(4)}
+            className={index === 4 ? 'menu__item-focused' : ''}>
+            Острые
+          </li>
+          <li
+            onClick={() => setIndex(5)}
+            className={index === 5 ? 'menu__item-focused' : ''}>
+            Закрытые
+          </li>
         </ul>
-      </div>
-    </header>
+      </nav>
+    </>
   );
 }
 
--- src/components/pizza-block.jsx
+++ src/components/pizza-block.jsx
@@ -2,18 +2,16 @@ import React, { useState } from 'react';
 
 function Pizzablock({ imageUrl, title, price }) {
   const [counter, setCounter] = useState(0);
-
-  function onClickAdd() {
-    setCounter(counter + 1);
-  }
   return (
     <div className="box">
       <img className="pizza-items" src={imageUrl} />
       <h2>{title}</h2>
-      <p>{price}$</p>
-      <a onClick={onClickAdd} className="btn">
-        Добавить: {counter}
-      </a>
+      <div className="box-item">
+        <p>{price} руб.</p>
+        <a onClick={() => setCounter(counter + 1)} className="btn">
+          Добавить: <b>{counter}</b>
+        </a>
+      </div>
     </div>
   );
 }
