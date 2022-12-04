b56e5e8a09fc87cde87d358652a9b0b3d7c26ea7
Magomed <zaurbkv@mail.ru>
2022-12-04 17:29:38 +0300

    new comm

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import '../App.css';
 
 function Home() {
   const [items, setItems] = React.useState([]);
+  const [index, setIndex] = React.useState(0);
   const [isLoader, setLoader] = React.useState(true);
 
   React.useEffect(() => {
@@ -18,13 +19,37 @@ function Home() {
   }, []);
 
   return (
-    <div className="wrapper">
-      {isLoader
-        ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-        : items.map((elem) => (
-            <Pizzablock title={elem.title} price={elem.price} imageUrl={elem.imageUrl} />
-          ))}
-    </div>
+    <>
+      <div className="categories">
+        <ul className="menu">
+          <li onClick={() => setIndex(0)} className={index === 0 ? 'menu__item' : ''}>
+            Все
+          </li>
+          <li onClick={() => setIndex(1)} className={index === 1 ? 'menu__item' : ''}>
+            Мясные
+          </li>
+          <li onClick={() => setIndex(2)} className={index === 2 ? 'menu__item' : ''}>
+            Вегетерианская
+          </li>
+          <li onClick={() => setIndex(3)} className={index === 3 ? 'menu__item' : ''}>
+            Гриль
+          </li>
+          <li onClick={() => setIndex(4)} className={index === 4 ? 'menu__item' : ''}>
+            Острые
+          </li>
+          <li onClick={() => setIndex(5)} className={index === 5 ? 'menu__item' : ''}>
+            Закрытые
+          </li>
+        </ul>
+      </div>
+      <div className="wrapper">
+        {isLoader
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((elem) => (
+              <Pizzablock title={elem.title} price={elem.price} imageUrl={elem.imageUrl} />
+            ))}
+      </div>
+    </>
   );
 }
 
